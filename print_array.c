de0baf95c2b216ccff570fb8b75798c96160bcc2
Romeo509 <romeoameku503@gmail>
2023-12-15 13:01:53 +0100

    betty fix: print_array.c

--- print_array.c
+++ print_array.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints an array of integers
- * @array: The array to be printed
- * @size: Number of elements in the array
- */
-void print_array(const int *array, size_t size) {
-    size_t i;
+* print_array - Prints an array of integers
+* @array: The array to be printed
+* @size: Number of elements in the array
+*/
+void print_array(const int *array, size_t size)
+{
+	size_t i;
 
-    for (i = 0; i < size; i++) {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
+	for (i = 0; i < size; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
 }
-
