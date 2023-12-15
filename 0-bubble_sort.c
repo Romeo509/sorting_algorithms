7d6298d9d3430b688e62a34f38f0ab257e10057b
Romeo509 <romeoameku503@gmail>
2023-12-15 15:07:21 +0100

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,12 +18,10 @@ void bubble_sort(int *array, size_t size)
 {
 			if (array[j] > array[j + 1])
 {
-				/* Swap the elements */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
-				/* Print the array after each swap */
 				print_array(array, size);
 			}
 		}
