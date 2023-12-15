b9f52246618a12368a273a81edc4a03641d94691
Romeo509 <romeoameku503@gmail>
2023-12-15 15:26:04 +0100

    fixed

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,29 +10,29 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int tmp;
-    if (!array || size < 2)
-        return;
- 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+size_t gap = 1, i, j;
+int tmp;
+if (!array || size < 2)
+return;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            tmp = array[i];
-            j = i;
-            while (j >= gap && array[j - gap] > tmp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
-            array[j] = tmp;
-        }
-        print_array(array, size);
-        gap = (gap - 1) / 3;
-    }
+while (gap < size / 3)
+gap = gap * 3 + 1;
+
+while (gap > 0)
+{
+for (i = gap; i < size; i++)
+{
+tmp = array[i];
+j = i;
+while (j >= gap && array[j - gap] > tmp)
+{
+array[j] = array[j - gap];
+j -= gap;
+}
+array[j] = tmp;
+}
+print_array(array, size);
+gap = (gap - 1) / 3;
+}
 }
 
