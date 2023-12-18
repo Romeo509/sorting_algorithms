a1f3ef742523d20e1441c81cb22f701935b56c56
DTAJ095 <albanjaures26@gmail.com>
2023-12-18 09:19:19 -0800

    added

--- sort.h
+++ sort.h
@@ -21,10 +21,14 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-
+void heap_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif /* SORT_H */
 
