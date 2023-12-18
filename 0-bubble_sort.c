bd3fd9b6c7082c8401a11978b58684c5c90bb246
DTAJ095 <albanjaures26@gmail.com>
2023-12-18 08:00:28 -0800

    corrected

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,23 +9,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, j;
-int temp;
+	size_t i, j, temp;
 
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - i - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-
-temp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = temp;
-
-print_array(array, size);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
-}
-}
-}
-
